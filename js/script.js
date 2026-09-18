783d610f925a6219eb4344bf74534ed799823e72
azuki <kuonnrh48951@outlook.jp>
2026-09-18 09:42:59 +0900

    スマホでメニューが閉じない不具合に対応

--- js/script.js
+++ js/script.js
@@ -5,3 +5,9 @@ humberger.addEventListener("click", () => {
   if (!headerMenu.classList.contains("loaded")) headerMenu.classList.add("loaded");
   headerMenu.classList.toggle("menu-open");
 });
+
+const menuCloseTarget = document.getElementById("menu-close-target");
+
+menuCloseTarget.addEventListener("click", () => {
+  headerMenu.classList.remove("menu-open");
+});
